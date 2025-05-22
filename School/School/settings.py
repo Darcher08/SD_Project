3917f9ea3d4698b4d86af75a847f39bcd0922f47
Darcher08 <148842609+Darcher08@users.noreply.github.com>
2025-05-21 23:29:30 -0600

    Ready for Deploy
    Ready for Deploy

--- School/School/settings.py
+++ School/School/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'School.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'school',
@@ -85,8 +85,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
